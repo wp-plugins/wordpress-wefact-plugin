67f271d3df9c8b120a49fb1d8dd73e518bc00c55
Tussendoor <Tussendoor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-21 07:43:48 +0000

    v 1.0
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-wefact-plugin/trunk@546878 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wefact.php
@@ -0,0 +1,34 @@
+<?php
+/*
+	Plugin Name: Wordpress WeFact plugin
+	Plugin URI: http://www.tussendoor.nl/wordpress-plugins/wefact-wordpress-plugin/
+	Description: WeFact Plugin. For the installation guide <a href="http://www.tussendoor.nl/wordpress-plugins/wefact-wordpress-plugin/">click here.</a> | Voor de installatie handleiding <a href="http://www.tussendoor.nl/wordpress-plugins/wefact-wordpress-plugin/">klik hier.</a>
+	Version: 1.0
+	Author: Tussendoor internet & marketing
+	Author URI: http://www.tussendoor.nl
+	
+*/
+
+require_once(dirname(__FILE__) . '/wefact_functions.php');
+if(is_admin())
+{
+	require_once(dirname(__FILE__) . '/wefact_admin.php');
+}
+
+class WeFact
+{
+	public function __construct()
+	{
+		register_activation_hook(__FILE__, array(&$this, 'activate'));
+	}
+	
+	public function activate()
+	{
+		if(!class_exists('SoapClient'))
+		{
+			die(__('Plugin requires that the Soap Client is installed on the server.', 'wefact'));
+		}
+	}
+}
+
+new WeFact();
